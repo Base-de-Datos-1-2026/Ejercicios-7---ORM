3d6af859ed2694a40f12aa12ebc03c4a19d6b19c
Luis Angel <langelgirona@gmail.com>
2026-05-11 21:34:24 -0600

    Set up Laravel project structure

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'Bookstore Eloquent Lab'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'UTC',
+    'locale' => 'es',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'es_ES',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+];
+
